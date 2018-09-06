d7106157f4999b272380d04bf89e5795e7b59c17
Jakedalus <jacquedalus@gmail.com>
2018-09-06 14:50:59 -0400

    Add more testing

--- server/server.js
+++ server/server.js
@@ -4,7 +4,29 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world.');
+    res.status(404).send({
+        error: 'Page not found',
+        name: 'ToDo App v1.0'
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.send([
+        {
+            name: 'Jake',
+            age: 36
+        },
+        {
+            name: 'Bob',
+            age: 39
+        },
+        {
+            name: 'Suzy',
+            age: 26
+        }
+    ]);
 });
 
 app.listen(3000);
+
+module.exports.app = app;
